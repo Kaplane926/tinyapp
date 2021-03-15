03734ea0db84d32cc3f3f870f3ee3002f460774a
Eli Kaplan <elikaplan@me.com>
2021-03-15 23:28:18 +0000

    began setting up EJS with a basic HTML skeleton

--- express_server.js
+++ express_server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const PORT = 8080;
 
+//requires ejs
 app.set("view engine", "ejs")
 
 
@@ -13,6 +14,11 @@ const urlDatabase = {
 app.get("/",(req, res)=>{
   res.send("Hello!");
 })
+//Since we're following the Express convention of using a views directory, we can take advantage of a useful EJS shortcut! EJS automatically knows to look inside the views directory for any template files that have the extension .ejs.
+app.get("/urls", (req, res)=>{
+  const templateVars = {urls: urlDatabase}
+res.render("urls_index", templateVars)
+});
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
