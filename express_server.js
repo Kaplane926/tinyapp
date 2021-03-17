1a482b49c5bea65f983741806b1dd513e0674366
Eli Kaplan <elikaplan@me.com>
2021-03-17 23:53:02 +0000

    somehow a bunch of stuff got deleted, rescued the code from git hub. Bless version control

--- express_server.js
+++ express_server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
 const PORT = 8080;
+const cookieParser = require("cookie-parser")
 
 function generateRandomString() {
   const Char = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
@@ -16,23 +17,33 @@ function generateRandomString() {
 app.set("view engine", "ejs")
 
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(cookieParser())
 
 const urlDatabase = {
   "b2xVn2": "http://lighthouslabs.ca",
   "9sm5xK": "http://google.com"
 };
 
+const users = {
+  "userRandomID": {
+    id: "userRandomID", 
+    email: "user@example.com", 
+    password: "purple-monkey-dinosaur"
+  },
+}
 
 app.get("/",(req, res)=>{
   res.send("Hello!");
 })
 //Since we're following the Express convention of using a views directory, we can take advantage of a useful EJS shortcut! EJS automatically knows to look inside the views directory for any template files that have the extension .ejs.
 app.get("/urls", (req, res)=>{
-  const templateVars = {urls: urlDatabase}
+  console.log("cookies " + req.cookies["username"])
+  const templateVars = {urls: urlDatabase, username: req.cookies["username"]}
 res.render("urls_index", templateVars)
 });
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  const templateVars = {username: req.cookies["username"]}
+  res.render("urls_new", templateVars);
 });
 app.post("/urls", (req, res) => {
   console.log(req.body.longURL);  // Log the POST request body to the console
@@ -41,12 +52,26 @@ app.post("/urls", (req, res) => {
   res.redirect(`/urls/${shortURL}`);         
 });
 app.get("/urls/:shortURL",(req, res)=>{
-  const templateVars = {shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]}
+  const templateVars = {shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL], username: req.cookies["username"]}
   res.render("urls_show", templateVars)
 });
 app.get("/u/:shortURL", (req, res) => {
   const longURL = urlDatabase[req.params.shortURL] // urlDatabase[shortURL] ??
   res.redirect(longURL);
+});
+app.post("/urls/login/", (req, res)=>{
+  res.cookie("username", req.body.username)
+  res.redirect("/urls")
+});
+app.post("/urls/logout/", (req, res)=>{
+  res.clearCookie("username")
+  res.redirect("/urls")
+});
+app.get("/register",(req, res)=>{
+res.render("urls_register")
+});
+app.post("/register", (req, res)=>{
+console.log(req.body)
 });
 app.post("/urls/:shortURL/delete",(req, res)=>{
   delete urlDatabase[req.params.shortURL]
